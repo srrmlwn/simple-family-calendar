a00f34c3d1750984495bcf0ba9e969cc3cc41138
sriram :zap <sriram.alwan@gmail.com>
2025-05-11 13:56:50 -0700

    More CSP updates

--- server/src/middleware/security.ts
+++ server/src/middleware/security.ts
@@ -8,13 +8,18 @@ export const securityHeaders = (req: Request, res: Response, next: NextFunction)
     res.setHeader(
         'Content-Security-Policy',
         "default-src 'self';" +
-        "script-src 'self' https://accounts.google.com https://apis.google.com https://www.gstatic.com 'unsafe-inline' 'unsafe-eval';" +
+        "script-src 'self' https://accounts.google.com https://apis.google.com https://www.gstatic.com 'unsafe-inline' 'unsafe-eval' https://*.google.com;" +
         "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;" +
         "font-src 'self' https://fonts.gstatic.com data:;" +
         "img-src 'self' data: https:;" +
-        "connect-src 'self' https://simple-family-calendar-8282627220c3.herokuapp.com https://accounts.google.com https://www.googleapis.com https://oauth2.googleapis.com;" +
-        "frame-src 'self' https://accounts.google.com;" +
-        "worker-src 'self' blob:;"
+        "connect-src 'self' https://famcal.ai https://simple-family-calendar-8282627220c3.herokuapp.com https://accounts.google.com https://www.googleapis.com https://oauth2.googleapis.com https://*.google.com;" +
+        "frame-src 'self' https://accounts.google.com https://*.google.com;" +
+        "worker-src 'self' blob:;" +
+        "object-src 'none';" +
+        "base-uri 'self';" +
+        "form-action 'self';" +
+        "frame-ancestors 'none';" +
+        "upgrade-insecure-requests;"
     );
 
     // Other security headers
