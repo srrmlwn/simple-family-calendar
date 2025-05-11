25b1ea4d80dd8a8aebef7e2b4ad43a18611b01e1
sriram :zap <sriram.alwan@gmail.com>
2025-05-11 14:00:33 -0700

    More CSP updates

--- server/src/middleware/security.ts
+++ server/src/middleware/security.ts
@@ -23,7 +23,6 @@ export const securityHeaders = (req: Request, res: Response, next: NextFunction)
     );
 
     // Other security headers
-    res.setHeader('X-Content-Type-Options', 'nosniff');
     res.setHeader('X-Frame-Options', 'DENY');
     res.setHeader('X-XSS-Protection', '1; mode=block');
     res.setHeader('Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
